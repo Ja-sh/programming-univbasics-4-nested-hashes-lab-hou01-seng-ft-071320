c937225088c16c6e49dee2c0b98bfa10179c7ae8
Joshua Moore <morejoshua99@hotmail.com>
2020-05-25 16:10:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,8 @@ def changing_alan
       }
     }
 
-programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash[:alan_kay][:known_for] << alans_new_info = programmer_hash2
+programmer_hash2
 
 end
 
