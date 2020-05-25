2f07b8651727a1e8b17fe845f98edd3b5d317d74
Joshua Moore <morejoshua99@hotmail.com>
2020-05-25 16:16:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
       }
     }
 
-programmer_hash[:alan_kay][:known_for]  == alans_new_info
+programmer_hash[:alan_kay][:known_for]  = alans_new_info
 end
 
 def dennis_ritchies_language
