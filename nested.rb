338fad83d869945c005f95a775ba9b6b57794395
Joshua Moore <morejoshua99@hotmail.com>
2020-05-25 16:02:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,8 +89,7 @@ programmer_hash
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
+alans_new_info  = "GUI"
 
 	programmer_hash = 
  		{
